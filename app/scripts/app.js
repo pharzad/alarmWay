b87e5eb3ab13c90abee3ae016c1d8becc9000799
farzad aziminia <farzadaziminia@farzads-MacBook-Pro.local>
2015-02-04 15:03:00 -0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('Wayalarm', ['ionic', 'config', 'Wayalarm.controllers', 'Wayalarm.directives'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+});
