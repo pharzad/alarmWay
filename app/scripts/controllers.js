49d8659e5f6d1c7944991927f21e7af7aa0b3a9e
Pharzad <pharzad@farzads-MacBook-Air.local>
2015-02-22 14:50:36 -0800

    Updating the current location

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,28 +2,25 @@
 
 angular.module('Wayalarm.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
+.controller('MapCtrl', function($scope, $ionicLoading, $interval ) {
   $scope.mapCreated = function(map) {
     $scope.map = map;
   };
-
-  $scope.centerOnMe = function () {
-    console.log('Centering');
+ $interval(function(){
     if (!$scope.map) {
       return;
     }
-
-    $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
+      navigator.geolocation.getCurrentPosition(function (pos) {
+     // console.log('Got pos', pos);
       $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
       $ionicLoading.hide();
     }, function (error) {
-      alert('Unable to get location: ' + error.message);
+      console.log('Unable to get location: ' + error.message);
     });
-  };
+  },200);
+//    $ionicLoading.show({
+//      content: 'Getting current location...',
+//      showBackdrop: false
+//    });
+
 });
